f3d943c9299626e561efcc78e1fa1be00cdc20b1
Janonez <95027402+sharkmini@users.noreply.github.com>
2022-10-26 21:12:47 +0800

    code
    code

--- 10_25_顺序表/10_25_顺序表/SeqList.c
+++ 10_25_顺序表/10_25_顺序表/SeqList.c
@@ -28,5 +28,50 @@ void SLDestroy(SL* ps)
 {
 	assert(ps);
 
+	if (ps->a)
+	{
+		free(ps->a);
+		ps->a = NULL;
+		ps->size = ps->capacity = 0;
+	}
 	
+
+}
+
+// 尾插
+void SLPushBack(SL* ps, SLDataType x)
+{
+	assert(ps);
+
+	// 判断空间是否满，进行扩容
+	if (ps->size == ps->capacity)
+	{
+		// 判断是否开辟空间，如果没开辟就给4个空间，如果开辟且满了，扩容为2倍
+		int newCapacity = ps->capacity == 0 ? 4 : 2 * ps->capacity;
+		SLDataType* tmp = (SLDataType*)realloc(ps->a, newCapacity * sizeof(SLDataType));
+		// 判断是否为空指针，如果是则说明开辟空间失败，并报错，退出程序
+		if (tmp == NULL)
+		{
+			perror("realloc");
+			exit(-1);
+		}
+		// 如果不是空指针，将其赋值给ps->a
+		ps->a = tmp;
+		// 最大容量更新
+		ps->capacity = newCapacity;
+	}
+
+	// 将要插入的值放入内存
+	ps->a[ps->size] = x;
+	// 更新现有的数据量
+	ps->size++;
+}
+
+// 尾删
+void SLPopBack(SL* ps)
+{
+	assert(ps);
+	// 暴力检查数据量是否为0
+	assert(ps->size > 0);
+	ps->size--;
 }
--- 10_25_顺序表/10_25_顺序表/Test.c
+++ 10_25_顺序表/10_25_顺序表/Test.c
@@ -33,10 +33,44 @@ void TestSeqList1()
 {
 	SL s1;
 	SLInit(&s1);
+	SLPushBack(&s1, 1);
+	SLPushBack(&s1, 2);
+	SLPushBack(&s1, 3);
+	SLPushBack(&s1, 4);
+	SLPushBack(&s1, 5);
+	SLPushBack(&s1, 6);
+	SLPushBack(&s1, 7);
+	SLPushBack(&s1, 8);
+	SLPushBack(&s1, 9);
+	SLPrint(&s1);
+	SLDestroy(&s1);
 }
+
+//int main()
+//{
+//	TestSeqList1();
+//	return 0;
+//}
 
 int main()
 {
-	TestSeqList1();
-	return 0;
+	int nums[] = { 0,1,2,2,3,0,4,2};
+	int numsSize = 8;
+	int val = 2;
+	int count = 0;
+    for (int i = 0; i < numsSize - count; i++)
+    {
+
+        if (nums[i] == val)
+        {
+            for (int j = i; j < numsSize - 1 - count; j++)
+            {
+                nums[j] = nums[j + 1];
+            }
+            count++;
+        }
+    }
+	for (int i = 0; i < numsSize - count; i++) 
+		printf("%d ",nums[i]);
+    return 0;
 }
