8d0c05728a8045c9439147b96e4c27adc3c8a8aa
Janonez <95027402+sharkmini@users.noreply.github.com>
2022-11-01 00:13:10 +0800

    code
    code

--- 10_30_单链表/10_30_单链表/Test.c
+++ 10_30_单链表/10_30_单链表/Test.c
@@ -1,10 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-void TestSList1()
-{
-
-}
-
+//void TestSList1()
+//{
+//	SLTNode* n1 = malloc();
+//	SLTNode* n2 = malloc();
+//	n1->next = n2;
+//
+//	SLTNode n1;
+//	SLTNode n2;
+//	n1.next = &n2;
+//}
 int main()
 {
 	TestSList1();
