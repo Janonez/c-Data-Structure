c0c0a8ee9188ca98527788c500d9298cca4327fc
Janonez <95027402+sharkmini@users.noreply.github.com>
2022-11-07 00:06:48 +0800

    code
    code

--- 11_6_链表OJ/11_6_链表OJ/Test.c
+++ 11_6_链表OJ/11_6_链表OJ/Test.c
@@ -117,3 +117,39 @@
 //    }
 //    return slow;
 //}
+
+
+//现有一链表的头指针 ListNode* pHead，给一定值x，编写一段代码将所有小于x的结点排在其余结点之前，且不能改变原来的数据顺序，返回重新排列后的链表的头指针。
+//class Partition {
+//public:
+//    ListNode* partition(ListNode* pHead, int x)
+//    {
+//        struct ListNode* lesshead, * lesstail, * greaterhead, * greatertail;
+//        lesshead = lesstail = (struct ListNode*)malloc(sizeof(struct ListNode));
+//        greaterhead = greatertail = (struct ListNode*)malloc(sizeof(struct ListNode));
+//        lesshead->next = greaterhead->next = NULL;
+//        struct ListNode* cur = pHead;
+//        while (cur)
+//        {
+//            if (cur->val >= x)
+//            {
+//                greatertail->next = cur;
+//                cur = cur->next;
+//                greatertail = greatertail->next;
+//            }
+//            else {
+//                lesstail->next = cur;
+//                cur = cur->next;
+//                lesstail = lesstail->next;
+//            }
+//        }
+//        if (greatertail != NULL)
+//            greatertail->next = NULL;
+//
+//        lesstail->next = greaterhead->next;
+//        pHead = lesshead->next;
+//        free(lesshead);
+//        free(greaterhead);
+//        return pHead;
+//    }
+//};
