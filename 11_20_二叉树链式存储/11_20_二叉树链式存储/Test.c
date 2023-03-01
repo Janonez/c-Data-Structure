d2e54ff0282f2a6a37b4111c64e00c7b1749db74
Janonez <906269785@qq.com>
2023-03-01 19:49:27 +0800

    二叉树的遍历

--- 11_20_二叉树链式存储/11_20_二叉树链式存储/Test.c
+++ 11_20_二叉树链式存储/11_20_二叉树链式存储/Test.c
@@ -56,26 +56,56 @@ BTNode* BuyNode(BTDataType x)
 	{
 		perror("malloc fail");
 		exit(-1);
-	}
+	}    
 
 	node->data = x;
 	node->left = node->right = NULL;
 	return node;
 }
 
-// 节点个数
+// 求二叉树节点个数
 int size = 0;
-void BinaryTreeSize(BTNode* root)
+void TreeSize(BTNode* root)
 {
 	if (root == NULL)
-	{
 		return;
-	}
 	size++;
-	BinaryTreeSize(root->left);
-	BinaryTreeSize(root->right);
+	TreeSize(root->left);
+	TreeSize(root->right);
 }
 
+int TreeSize2(BTNode* root)
+{
+	return root == NULL ? 0 : TreeSize2(root->left) + TreeSize2(root->right) + 1;
+}
+
+// 叶子节点个数
+int TreeLeafSize(BTNode* root)
+{
+	if (root == NULL)
+		return 0;
+	return (root->left == NULL && root->right == NULL) ? 1 : TreeLeafSize(root->left) + TreeLeafSize(root->right);
+}
+
+// 求树的高度/深度
+int TreeHeight(BTNode* root)
+{
+	if (root == NULL)
+		return 0;
+	int leftHeight = TreeHeight(root->left);
+	int rightHeight = TreeHeight(root->right);
+	return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+}
+
+// 求第k层节点个数 第k层
+int TreeLevelSize(BTNode* root, int k)
+{
+	if (root == NULL)
+		return 0;
+	if (k == 1)
+		return 1;
+	return TreeLevelSize(root->left, k - 1) + TreeLevelSize(root->right, k - 1);
+}
 int main()
 {
 	BTNode* n1 = BuyNode(1);
@@ -84,11 +114,13 @@ int main()
 	BTNode* n4 = BuyNode(4);
 	BTNode* n5 = BuyNode(5);
 	BTNode* n6 = BuyNode(6);
+	BTNode* n7 = BuyNode(7);
 	n1->left = n2;
 	n1->right = n4;
 	n2->left = n3;
 	n4->left = n5;
 	n4->right = n6;
+	n3->left = n7;
 
 	PrevOrder(n1);
 	printf("\n");
@@ -99,9 +131,15 @@ int main()
 	PostOrder(n1);
 	printf("\n");
 
-	size = 0;
-	BinaryTreeSize(n1);
-	printf("BinaryTreeSize:%d\n", size);
+	/*size = 0;
+	TreeSize(n1);
+	printf("TreeSize:%d\n", size);
+	printf("TreeSize:%d\n", TreeSize2(n1));
+	printf("TreeSize:%d\n", TreeSize2(n1));
+	printf("TreeSize:%d\n", TreeSize2(n1));*/
 
+	printf("TreeLeafSize:%d\n", TreeLeafSize(n1));
+	
+	printf("TreeHeight:%d\n", TreeHeight(n1));
 	return 0;
 }
