c91db14757c0498d5ff098db2b18cc8357283d5e
Janonez <906269785@qq.com>
2023-03-07 23:02:57 +0800

    希尔排序

--- 11_27_排序/11_27_排序/Sort.c
+++ 11_27_排序/11_27_排序/Sort.c
@@ -1,6 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "Sort.h"
 
+void PrintArray(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
+
 void InsertSort(int* a, int n)
 {
 	for (int i = 0; i < n - 1; i++)
@@ -22,4 +31,58 @@ void InsertSort(int* a, int n)
 		a[end + 1] = tmp;
 	}
 	
+}
+
+void ShellSort(int* a, int n)
+{
+	// gap > 1  Ô¤ÅÅÐò
+	// gap == 1 ²åÈëÅÅÐò
+	int gap = n;
+	while (gap > 1)
+	{
+		//gap = gap / 2;
+		gap = gap / 3 + 1;
+		for (int i = 0; i < n - gap; i++)
+		{
+			int end = i;
+			int tmp = a[end + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+		//PrintArray(a, n);
+
+	}
+	/*int gap = 3;
+	for (int j = 0; j < gap; j++)
+	{
+		for (int i = j; i < n - gap; i += gap)
+		{
+			int end = i;
+			int tmp = a[end + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+	}*/
 }
--- 11_27_排序/11_27_排序/Sort.h
+++ 11_27_排序/11_27_排序/Sort.h
@@ -1,4 +1,7 @@
 #pragma once
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 void InsertSort(int* a, int n);
+void PrintArray(int* a, int n);
+void ShellSort(int* a, int n);
